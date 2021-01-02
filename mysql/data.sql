9b5561f165c8ec8299f4f0c011933d6888ebb6ca
Tri Le <thanhtrixx@gmail.com>
2021-01-02 19:36:06 +0700

    + Add MySql sample data

---
+++ mysql/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO `user` (`id`, `name`, `password`, `phone`)
+VALUES
+	(1, 'trile', '123123', '123456789'),
+	(2, 'test', '321321', '987654321');
+
+INSERT INTO `data_voucher` (`id`, `create_time`, `user_id`, `voucher`)
+VALUES
+	(1, '2021-01-02 12:29:47.243000', 1, 'BVRIJHGT'),
+	(2, '2021-01-02 12:29:51.967000', 1, 'APTVVLGB'),
+	(3, '2021-01-02 12:29:55.007000', 1, 'PHRLMUKW');
