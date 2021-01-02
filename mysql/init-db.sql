f386257cb549d0907642b4eeecd7bf41bdca0237
Tri Le <thanhtrixx@gmail.com>
2021-01-03 01:17:15 +0700

    + Config docker-compose + Update MySql init script

--- mysql/init-db.sql
+++ mysql/init-db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `data_voucher` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `create_time` datetime(6) DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   `voucher` varchar(64) DEFAULT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE `data_voucher` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) DEFAULT NULL,
   `password` varchar(64) DEFAULT NULL,
   `phone` varchar(64) DEFAULT NULL,
